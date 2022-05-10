71241c9989217d3f3efcce830b16f28246754ee2
AKASH <60318943+AkashDallen@users.noreply.github.com>
2022-05-10 08:02:27 +0530

    Delete prog directory

--- prog/assignment programs/pattern.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-void main()
-{
-    printf("1\t\t  1\n");
-    printf("1,2\t\t2,1\n");
-    printf("1,2,3\t      3,2,1\n");
-    printf("1,2,3,4     4,3,2,1\n");
-    printf("1,2,3,4,5,5,4,3,2,1\n");
-}
